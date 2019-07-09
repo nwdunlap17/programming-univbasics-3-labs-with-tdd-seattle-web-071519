cdefe09da3a8b6d938001a4a59575e6eba46d47e
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-09 20:58:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,4 @@ second_number = 12
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-dividend = first_number * 1.0 / second_number
+quotient = first_number * 1.0 / second_number
